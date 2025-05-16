9e6546b0dbaf7dd76c1cc642613d31276ead3dc4
Yoru-cyber <carlosmendez170210@gmail.com>
2025-05-16 12:06:59 -0400

    feat: program now checks the domain disponibility fix: change some inconsistent function names

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <curl/curl.h>
+#include <curl/easy.h>
 #include <fstream>
 #include <iostream>
-#include <list>
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 struct Source
@@ -12,14 +12,14 @@ struct Source
   std::string baseUrl;
 };
 NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE (Source, name, lang, id, baseUrl)
-struct RepoJson
+struct Repo
 {
   std::string name;
   std::string lang;
   int nsfw;
   std::vector<Source> sources;
 };
-NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE (RepoJson, name, lang, nsfw, sources)
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE (Repo, name, lang, nsfw, sources)
 
 json
 read_json (std::string json_file_path)
@@ -28,23 +28,37 @@ read_json (std::string json_file_path)
   json data = json::parse (f);
   return data;
 }
-void
-callRepoUrl (std::string url)
-{
-  std::cout << url << std::endl;
-}
 size_t
-write_data (void *ptr, size_t size, size_t nmemb, FILE *stream)
-{
-  size_t written = fwrite (ptr, size, nmemb, stream);
-  return written;
-}
-size_t
-WriteCallback (void *contents, size_t size, size_t nmemb, std::string *userp)
+write_callback (void *contents, size_t size, size_t nmemb, std::string *userp)
 {
   size_t total_size = size * nmemb;
   userp->append (static_cast<char *> (contents), total_size);
   return total_size;
+}
+void
+call_repo_url (std::string url, CURL *curl)
+{
+  long http_response_code = 0;
+  curl_easy_setopt (curl, CURLOPT_URL, url.c_str ());
+  curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);
+  curl_easy_getinfo (curl, CURLINFO_HTTP_CODE, &http_response_code);
+  CURLcode res = curl_easy_perform (curl);
+  if (res != CURLE_OK)
+    {
+      std::fprintf (stderr, "curl_easy_perform() failed:%s\n",
+                    curl_easy_strerror (res));
+    }
+  else
+    {
+      if (http_response_code == 200)
+        {
+          std::cout << url << " is available" << std::endl;
+        }
+      else
+        {
+          std::cout << url << " Res code: " << http_response_code << std::endl;
+        }
+    }
 }
 int
 main (int argc, char *argv[])
@@ -60,7 +74,7 @@ main (int argc, char *argv[])
     {
       CURLcode res;
       curl_easy_setopt (curl, CURLOPT_URL, file_url.c_str ());
-      curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+      curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);
       curl_easy_setopt (curl, CURLOPT_WRITEDATA, &response);
       res = curl_easy_perform (curl);
       if (res != CURLE_OK)
@@ -80,12 +94,12 @@ main (int argc, char *argv[])
           domains_file << response;
           domains_file.close ();
           json data = read_json (file_name);
-          auto repo_json = data.template get<std::vector<RepoJson>> ();
+          auto repo_json = data.template get<std::vector<Repo>> ();
           for (auto repo : repo_json)
             {
               if (repo.lang == "es")
                 {
-                  callRepoUrl (repo.sources[0].baseUrl);
+                  call_repo_url (repo.sources[0].baseUrl, curl);
                 }
             }
           curl_easy_cleanup (curl);
